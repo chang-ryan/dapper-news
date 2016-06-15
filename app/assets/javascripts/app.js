ae4337114e064a19bf66458f87e8d04c4d52222e
Ryan Chang <ry4nch4ng@gmail.com>
2016-06-14 20:50:20 -0700

    login/register states and controller

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -32,6 +32,26 @@
           }]
         }
       });
+      $stateProvider.state('login', {
+        url: '/login',
+        templateUrl: 'auth/_login.html',
+        controller: 'AuthController',
+        onEnter: ['$state', 'Auth', function($state, Auth) {
+          Auth.currentUser().then(function () {
+            $state.go('home');
+          })
+        }]
+      });
+      $stateProvider.state('register', {
+        url: '/register',
+        templateUrl: 'auth/_register.html',
+        controller: 'AuthController',
+        onEnter: ['$state', 'Auth', function($state, Auth) {
+          Auth.currentUser().then(function () {
+            $state.go('home');
+          })
+        }]
+      });
 
       $urlRouterProvider.otherwise('home');
     }
